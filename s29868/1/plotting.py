9e401d0f77663f1f7cf1ddc4cead21d04eebeb93
Kiryl Sankouski <sankovsky85@gmail.com>
2025-10-02 13:12:18 +0200

    Lab 1: Podstawy Pythona i narzędzi ML | Last task added

--- s29868/1/plotting.py
+++ s29868/1/plotting.py
@@ -5,11 +5,9 @@ df = pd.read_csv('data.csv')
 print(df.head(5))
 
 
-
-
-
 plt.hist(df["subscription_type"], bins=4, edgecolor='black')
 plt.title("Number of subscriptions by type")
 plt.xlabel("Subscription Type")
 plt.ylabel("Number")
-plt.show()
+plt.savefig("plot.png")
+plt.close()
