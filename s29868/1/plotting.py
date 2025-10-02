7b42a4a4540672ca7b0064903506753ffef02d02
Kiryl Sankouski <sankovsky85@gmail.com>
2025-10-02 12:42:00 +0200

    Lab 1: Podstawy Pythona i narzędzi ML

---
+++ s29868/1/plotting.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read_csv('data.csv')
+print(df.head(5))
+
+
+
+
+
+plt.hist(df["subscription_type"], bins=4, edgecolor='black')
+plt.title("Number of subscriptions by type")
+plt.xlabel("Subscription Type")
+plt.ylabel("Number")
+plt.show()
