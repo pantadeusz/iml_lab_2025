70de492a3cd6c6541958e73548c28bc428080d4e
Myroslav Natalchenko <99477515+MyroslavNatalchenko@users.noreply.github.com>
2025-10-02 13:10:00 +0200

    Lab1

--- s30023/01/syntax_issues.py
+++ s30023/01/syntax_issues.py
@@ -1,16 +1,17 @@
 import sys
 import pandas as pd
 import matplotlib.pyplot as plt
+import argparse
 
 if __name__ == '__main__':
-    if len(sys.argv) != 5:
-        print('Usage: python syntax_issues.py <file.csv> <column name> <min> <max>')
-        sys.exit(1)
-
-    filename = sys.argv[1]
-    column_name = sys.argv[2]
-    min = int(sys.argv[3])
-    max = int(sys.argv[4])
+    # if len(sys.argv) != 5:
+    #     print('Usage: python syntax_issues.py <file.csv> <column name> <min> <max>')
+    #     sys.exit(1)
+    #
+    # filename = sys.argv[1]
+    # column_name = sys.argv[2]
+    # min = int(sys.argv[3])
+    # max = int(sys.argv[4])
 
     df = pd.read_csv(filename)
     df = df[df[column_name].between(min, max)]
