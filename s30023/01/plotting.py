e8198b2981c0018d60ff3269744f493b54ce29c5
Myroslav Natalchenko <99477515+MyroslavNatalchenko@users.noreply.github.com>
2025-10-02 12:49:09 +0200

    Lab 1: Podstawy Pythona i narzędzi ML

--- s30023/01/plotting.py
+++ s30023/01/plotting.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-df = pd.read_csv('BMW.csv')
+df = pd.read_csv('data.csv')
 
-plt.plot(df['Year'], df['Sales_Volume'])
-plt.title('Przykładowy wykres')
-plt.xlabel('X')
-plt.ylabel('Y')
+plt.hist(df[' genre'])
+plt.title('Songs in dataset by genre')
+plt.xlabel('genre')
+plt.ylabel('Amount of songs')
 plt.show()
+plt.savefig("test_histogram.png")
