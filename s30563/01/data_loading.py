e0d0c39ef36b0c9e0d187bcdbf7e37b0c9a27e15
Oskar Tarka <oskar0208@gmail.com>
2025-10-03 19:47:21 +0200

    Poprawy i ostatnie zadanie

--- s30563/01/data_loading.py
+++ s30563/01/data_loading.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+# https://www.kaggle.com/datasets/ayeshaimran123/top-youtube-music-hits-2025
 df = pd.read_csv("youtube-top-100-songs-2025.csv")
 
 pd.set_option("display.max_columns", None)
