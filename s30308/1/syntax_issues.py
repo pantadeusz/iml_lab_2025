5bc2dbbc4893bf663ffa1638c4a2567b05abba95
Wiktor Wilinski <wilinskiwiktor.ww@gmail.com>
2025-10-08 14:39:30 +0200

    Poprawiona wersja. Pobranie dataseta odbywa dzięki KaggleHub. Plik jest zapisywany w katalogu labu

--- s30308/1/syntax_issues.py
+++ s30308/1/syntax_issues.py
@@ -1,10 +1,8 @@
 import argparse
-
 import pandas as pd
 from matplotlib import pyplot as plt
 
 parser = argparse.ArgumentParser()
-
 parser.add_argument("sciezka_do_pliku_csv")
 parser.add_argument("nazwa_kolumny")
 parser.add_argument("min_wartosc")
@@ -15,9 +13,11 @@ args = parser.parse_args()
 df = pd.read_csv(args.sciezka_do_pliku_csv)
 col = args.nazwa_kolumny
 
-filtered_col = df.query(f'{col} > {args.min_wartosc} and {col} < {args.max_wartosc}')
-#filtered_col = df[df[col] > args.min_wartosc]
-df = pd.DataFrame(filtered_col)
+filtered_df = df.query(f"{col} > {args.min_wartosc} and {col} < {args.max_wartosc}")
 
-df.hist()
-plt.show()
+filtered_df[col].hist()
+plt.title(f"Histogram kolumny '{col}'")
+plt.xlabel(col)
+plt.ylabel("Liczba wystąpień")
+
+plt.savefig("histogram.png")
