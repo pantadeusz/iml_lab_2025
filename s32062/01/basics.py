186900ef4efa8f22428a79721f9db58d90f53c92
Mikołaj Jassowicz <s32062@pjwstk.edu.pl>
2025-10-09 13:38:59 +0200

    Lab 1: Podstawy Pythona i narzędzi ML - poprawione

--- s32062/01/basics.py
+++ s32062/01/basics.py
@@ -1,11 +1,12 @@
 x = "Mikołaj Jassowicz"
-list = [1,2,3,4,5]
+list = [1, 2, 3, 4, 5]
 
 for i in list:
     print(i)
 
-def sum(a,b):
-    return a+b
 
-print(sum(1,2))
+def sum(a, b):
+    return a + b
 
+
+print(sum(1, 2))
--- s32062/01/data_loading.py
+++ s32062/01/data_loading.py
@@ -1,8 +1,9 @@
 import pandas as pd
-# Przykładowe dane
-data = {'kolumna1': [1, 2, 3], 'kolumna2': [4, 5, 6]}
-df = pd.DataFrame(data)
-print(df.head())
 
-df1 = pd.read_csv('lung_cancer_dataset.csv')
+# Przykładowe dane
+data = {"kolumna1": [1, 2, 3], "kolumna2": [4, 5, 6]}
+df = pd.DataFrame(data)
+print(df[0])
+
+df1 = pd.read_csv("lung_cancer_dataset.csv")
 print(df1.head())
