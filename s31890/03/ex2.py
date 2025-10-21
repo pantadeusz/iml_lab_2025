b8998553882dbce78ef6190a217db4661389b62a
johnyboro <bor.jan.g.2018@gmail.com>
2025-10-21 15:04:20 +0200

    Start lab3

--- s31890/03/ex2.py
+++ s31890/03/ex2.py
@@ -3,7 +3,7 @@ from sklearn.svm import SVC  # pyright: ignore[reportMissingImports]
 from sklearn.metrics import roc_curve, auc, precision_recall_curve  # pyright: ignore[reportMissingImports]
 from sklearn.linear_model import LogisticRegression  # pyright: ignore[reportMissingImports]
 import matplotlib.pyplot as plt  # pyright: ignore[reportMissingImports]
-from sklearn.datasets import load_breast_cancer  # pyright: ignore[reportMissingImports]
+from sklearn.datasets import make_classification  # pyright: ignore[reportMissingImports]
 from sklearn.model_selection import train_test_split  # pyright: ignore[reportMissingImports]
 
 models = [
@@ -13,11 +13,19 @@ models = [
 ]
 
 # Załaduj dane
-data = load_breast_cancer()
-X, y = data.data, data.target
+X, y = make_classification(
+    n_samples=1000,
+    n_features=20,
+    n_informative=2,
+    n_redundant=10,
+    n_clusters_per_class=1,
+    # weights=[0.9, 0.1],
+    flip_y=0,
+    random_state=1
+)
 # Podziel dane
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
+    X, y, test_size=0.2, random_state=1
 )
 
 plt.figure(figsize=(12, 5))
@@ -31,12 +39,18 @@ for name, model in models:
     roc_auc = auc(fpr, tpr)
     plt.subplot(1, 2, 1)
     plt.plot(fpr, tpr, label=f"{name} (AUC = {roc_auc:.2f})")
+    
+    print("ROC:")
+    print(f"{name}: AUC = {roc_auc:.2f}\n")
 
     # PR
     precision, recall, _ = precision_recall_curve(y_test, y_proba)
     pr_auc = auc(recall, precision)
     plt.subplot(1, 2, 2)
     plt.plot(recall, precision, label=f"{name} (AUC = {pr_auc:.2f})")
+    
+    print("Precision-Recall:")
+    print(f"{name}: AUC = {pr_auc:.2f}\n")
 
 plt.subplot(1, 2, 1)
 plt.plot([0, 1], [0, 1], "k--")
