83c67b86094a1ba8b0a03bbcff5936fc95fe8cb5
KacperM <miakinkokacper@gmail.com>
2025-10-23 13:07:22 +0200

    lab 1 zakonczony

---
+++ s30131/01/basics.py
@@ -0,0 +1,11 @@
+name = "Kacper"
+
+list = [1, 2, 3, 4, 5]
+
+for x in list:
+    print(x)
+
+def function(x, y):
+    print(x + y)
+
+function(1,2)
---
+++ s30131/01/data_loading.py
@@ -0,0 +1,9 @@
+import pandas as pd
+file_path = "insurance.csv"
+df = pd.read_csv(file_path)
+print(df.head())
+
+
+
+
+
