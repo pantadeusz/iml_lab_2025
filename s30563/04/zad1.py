64064d7c3b7f6910f725b774cee04dcb0a4b6f45
Oskar Tarka <oskar0208@gmail.com>
2025-10-30 11:55:34 +0100

    wygląd kodu poprawiony (black)

--- s30563/04/zad1.py
+++ s30563/04/zad1.py
@@ -10,7 +10,9 @@ data = load_breast_cancer()
 X = data.data
 y = data.target
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
 
 classic_model = RandomForestClassifier(random_state=42)
 
@@ -23,30 +25,35 @@ scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
-nn_model = tf.keras.models.Sequential([
-    tf.keras.layers.Dense(32, activation='relu', input_shape=(30,)),
-    tf.keras.layers.Dropout(0.2),
-    # tf.keras.layers.Dense(32, activation='relu'),
-    # tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Dense(16, activation='relu'),
-    # tf.keras.layers.Dense(8, activation='relu'),
-    tf.keras.layers.Dense(1, activation='sigmoid')
-])
-
-nn_model.compile(
-    optimizer="adam",
-    loss='binary_crossentropy',
-    metrics=['accuracy',
-             tf.keras.metrics.Precision(name='precision'),
-             tf.keras.metrics.Recall(name='recall'),
+nn_model = tf.keras.models.Sequential(
+    [
+        tf.keras.layers.Dense(32, activation="relu", input_shape=(30,)),
+        tf.keras.layers.Dropout(0.2),
+        # tf.keras.layers.Dense(32, activation='relu'),
+        # tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(16, activation="relu"),
+        # tf.keras.layers.Dense(8, activation='relu'),
+        tf.keras.layers.Dense(1, activation="sigmoid"),
     ]
 )
 
-training = nn_model.fit(X_train, y_train, epochs=30, batch_size=16, validation_split=0.1, verbose=0)
+nn_model.compile(
+    optimizer="adam",
+    loss="binary_crossentropy",
+    metrics=[
+        "accuracy",
+        tf.keras.metrics.Precision(name="precision"),
+        tf.keras.metrics.Recall(name="recall"),
+    ],
+)
+
+training = nn_model.fit(
+    X_train, y_train, epochs=30, batch_size=16, validation_split=0.1, verbose=0
+)
 history_df = pd.DataFrame(training.history)
 
 history_df.index = history_df.index + 1
-history_df.index.name = 'Epoka'
+history_df.index.name = "Epoka"
 
 history_df.to_csv("training.csv", index=False)
 
