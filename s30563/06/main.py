090483196535db9e7f4e4b95edb6be11efd9e5ce
Oskar Tarka <oskar0208@gmail.com>
2025-11-12 23:21:15 +0100

    tuner

--- s30563/06/main.py
+++ s30563/06/main.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 import keras
+import keras_tuner as kt
 import numpy as np
 import tensorflow_datasets as tfds
 from keras_tuner.src.backend.io import tf
@@ -31,15 +32,53 @@ def prepare_dataset(dataset, batch_size=32, img_size=(128,128), shuffle=False):
 
     return dataset
 
-def create_model(input_shape, initializer='glorot_uniform', activation='relu', optimizer='adam'):
-    model = keras.Sequential([
-        keras.layers.Flatten(input_shape=input_shape),
-        keras.layers.Dense(128, activation=activation, initializer=initializer),
-        keras.layers.Dense(64, activation=activation),
-        keras.layers.Dense(32, activation=activation),
-        keras.layers.Dense(3, activation='softmax')
-    ])
-    model.compile(optimizer=optimizer, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+# def create_model(input_shape, initializer='glorot_uniform', activation='relu', optimizer='adam'):
+#     model = keras.Sequential([
+#         keras.layers.Flatten(input_shape=input_shape),
+#         keras.layers.Dense(128, activation=activation, kernel_initializer=initializer),
+#         keras.layers.Dense(64, activation=activation, kernel_initializer=initializer),
+#         keras.layers.Dropout(0.2),
+#         keras.layers.Dense(32, activation=activation, kernel_initializer=initializer),
+#         keras.layers.Dense(16, activation=activation, kernel_initializer=initializer),
+#         keras.layers.Dense(3, activation='softmax')
+#     ])
+#     model.compile(optimizer=optimizer, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+#     return model
+
+
+def build_model(hp, input_shape):
+    model = keras.models.Sequential()
+
+    activations = ["relu", "tanh", "elu"]
+    initializers = ["glorot_uniform", "he_normal", "he_uniform"]
+    optimizers = ["adam", "rmsprop", "sgd"]
+
+    model.add(keras.layers.Flatten(input_shape=input_shape))
+
+    for i in range(hp.Int("num_layers", 2, 6)):
+        model.add(
+            keras.layers.Dense(
+                units=hp.Int(f"units_{i+1}", min_value=16, max_value=512, step=32),
+                activation=hp.Choice("activation", activations),
+            )
+        )
+
+    if hp.Boolean("dropout"):
+        model.add(keras.layers.Dropout(rate=0.25))
+
+    model.add(keras.layers.Dense(3, activation="softmax"))
+
+    opt_choice = hp.Choice("optimizer", optimizers)
+    lr = hp.Float("learning_rate", min_value=1e-4, max_value=1e-2, sampling="log")
+
+    if opt_choice == "adam":
+        optimizer = keras.optimizers.Adam(learning_rate=lr)
+    elif opt_choice == "rmsprop":
+        optimizer = keras.optimizers.RMSprop(learning_rate=lr)
+    else:
+        optimizer = keras.optimizers.SGD(learning_rate=lr)
+
+    model.compile(optimizer=optimizer, loss="sparse_categorical_crossentropy", metrics=["accuracy"])
     return model
 
 def save_model(model, path):
@@ -54,10 +93,15 @@ def save_model(model, path):
     model.save(save_model_path)
 
 def main():
+    epochs, max_trials, executions_per_trial = 30, 2, 5
+
     if len(sys.argv) > 1:
-        epochs = int(sys.argv[1])
-    else:
-        epochs = 10
+        try:
+            epochs = int(sys.argv[1])
+            max_trials = int(sys.argv[2])
+            executions_per_trial = int(sys.argv[3])
+        except (IndexError, ValueError):
+            print("Niepoprawne argumenty. Używam wartości domyślnych: 30, 2, 5")
 
     train_ds, val_ds, test_ds = get_dataset()
     train_ds = prepare_dataset(train_ds, shuffle = True)
@@ -67,17 +111,28 @@ def main():
     sample_img, _ = next(iter(train_ds))
     input_shape = sample_img.shape[1:]
 
-    model = create_model(input_shape)
-    model.fit(train_ds, validation_data=val_ds, epochs=epochs)
 
-    test_loss, test_acc = model.evaluate(test_ds)
-    print(f"Test accuracy: {test_acc:.4f}")
+    tuner = kt.RandomSearch(
+        lambda hp: build_model(hp, input_shape=input_shape),
+        objective="val_accuracy",
+        max_trials=max_trials,
+        executions_per_trial=executions_per_trial,
+        directory="tunning",
+        overwrite=True,
+        project_name=f"{epochs}_epochs_{max_trials}_trials_{executions_per_trial}_executions_per_trial",
+    )
 
-    y_true = np.concatenate([y.numpy() for x, y in test_ds], axis=0)
-    y_pred = np.argmax(model.predict(test_ds), axis=1)
+
+    tuner.search(train_ds, validation_data=val_ds, epochs=epochs, verbose=0)
+
+    best_model = tuner.get_best_models(num_models=1)[0]
+    best_model.summary()
+
+    y_true = np.concatenate([y for x, y in test_ds], axis=0)
+    y_pred = np.argmax(best_model.predict(test_ds), axis=1)
     print(classification_report(y_true, y_pred))
 
-    save_model(model, f"./models/v1_{epochs}")
+    save_model(best_model, f"./models/tuner_model_{epochs}_max_trials_{max_trials}_executions_per_trial_{executions_per_trial}")
 
 
 if __name__ == "__main__":
