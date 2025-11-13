19ffb5f9379ea7b3189a0473ecffa8b80470dfb5
noyex <153921909+noyex@users.noreply.github.com>
2025-11-13 13:09:05 +0100

    Update klasyfikacja.py

--- s30565/06/lab6/klasyfikacja.py
+++ s30565/06/lab6/klasyfikacja.py
@@ -1,44 +1,32 @@
 import tensorflow as tf
 import numpy as np
 import tensorflow_datasets as tfds
+import os
 
-print("Wczytywanie modelu z pliku 'best_beans_model.keras'...")
+image_path = ''
+
+if not os.path.exists(image_path):
+    print("Nie znaleziono obrazu!")
+    exit()
+
 model = tf.keras.models.load_model('best_beans_model.keras')
-print("Model wczytany.")
 
+def load_image(image_path):
+    img = tf.keras.utils.load_img(image_path, target_size=(128, 128), color_mode='rgb')
+    img_array = tf.keras.utils.img_to_array(img)
+    img_array = img_array / 255.0
+    img_array = tf.expand_dims(img_array, axis=0)
+    return img_array
 
-def preprocess_image(image, label):
-    image = tf.image.resize(image, (128, 128))
-    image = tf.cast(image, tf.float32) / 255.0
-    return image, label
+image_for_prediction = load_image(image_path)
 
-ds_test = tfds.load('beans', split='test', as_supervised=True)
-ds_test = ds_test.map(preprocess_image).batch(32).take(1)
-
-for images, labels in ds_test:
-    sample_image = images[0]
-    true_label_index = labels[0].numpy()
-    break
-
-image_for_prediction = tf.expand_dims(sample_image, axis=0)
-
-class_names = ['Angular Leaf Spot', 'Bean Rust', 'Healthy']
-true_label_name = class_names[true_label_index]
-
-
-
+class_names = ['Angular Leaf Spot (Plamistość)', 'Bean Rust (Rdza)', 'Healthy (Zdrowa)']
 predictions = model.predict(image_for_prediction)
-
 predicted_class_index = np.argmax(predictions[0])
 predicted_class_name = class_names[predicted_class_index]
 confidence = np.max(predictions[0]) * 100
 
-print("\n--- Wyniki klasyfikacji ---")
-print(f"Prawdziwa etykieta: {true_label_name}")
-print(f"Przewidziana etykieta: {predicted_class_name}")
-print(f"Pewność (confidence): {confidence:.2f}%")
-
-if predicted_class_name == true_label_name:
-    print("\nPredykcja jest poprawna! :)")
-else:
-    print("\nPredykcja jest niepoprawna. :(")
+print("\n--- Wynik diagnozy ---")
+print(f"Diagnoza: {predicted_class_name}")
+print(f"Pewność modelu: {confidence:.2f}%")
+print(f"Surowe wyniki (prawdopodobieństwa): {predictions[0]}")
