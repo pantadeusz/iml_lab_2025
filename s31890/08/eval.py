4f52a4709da1f4524881fcb883a20999004e3fe7
johnyboro <bor.jan.g.2018@gmail.com>
2025-11-27 22:10:33 +0100

    minor changes

--- s31890/08/eval.py
+++ s31890/08/eval.py
@@ -8,7 +8,13 @@ BATCH_SIZE = 128
 test_ds = data.prepare(test_ds, info, batch_size=BATCH_SIZE, augment=True)
 
 loaded_model = keras.saving.load_model("best_tuner_model_no_augment.keras")
+loaded_model_augment = keras.saving.load_model("model_retrained_augment.keras")
 
 results = loaded_model.evaluate(test_ds)
+results_augment = loaded_model_augment.evaluate(test_ds)
 
+print("Standard model tested on augmented data without retraining:")
 print("test loss, test acc: ", results)
+
+print("Retrained model:")
+print("test loss, test acc: ", results_augment)
---
+++ s31890/08/retrain.py
@@ -0,0 +1,17 @@
+import data
+import keras
+
+train_ds, test_ds, info = data.get_mnist()
+data.print_info(info)
+
+BATCH_SIZE = 128
+test_ds = data.prepare(test_ds, info, batch_size=BATCH_SIZE, augment=True)
+
+loaded_model = keras.saving.load_model("best_tuner_model_no_augment.keras")
+# Retrain the same model
+history = loaded_model.fit(
+    train_ds, batch_size=BATCH_SIZE, epochs=30, validation_data=test_ds
+)
+
+loaded_model.save("model_retrained_augment.keras")
+print("Saved the retrained model")
