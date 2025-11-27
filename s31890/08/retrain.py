60ea5113a4cd3a3a797fced02607537bd3939de1
johnyboro <bor.jan.g.2018@gmail.com>
2025-11-27 22:23:54 +0100

    minor changes

--- s31890/08/retrain.py
+++ s31890/08/retrain.py
@@ -5,7 +5,10 @@ train_ds, test_ds, info = data.get_mnist()
 data.print_info(info)
 
 BATCH_SIZE = 128
-test_ds = data.prepare(test_ds, info, batch_size=BATCH_SIZE, augment=True)
+test_ds = data.prepare(test_ds, info, batch_size=BATCH_SIZE, augment=False)
+train_ds = data.prepare(
+    train_ds, info, batch_size=BATCH_SIZE, shuffle=True, augment=True
+)
 
 loaded_model = keras.saving.load_model("best_tuner_model_no_augment.keras")
 # Retrain the same model
