6a12b01265567b87dc3b28b02120b321737dd304
Myroslav Natalchenko <99477515+MyroslavNatalchenko@users.noreply.github.com>
2025-12-11 09:44:48 +0100

    Correction of mistake

--- s30023/10/lab10_text_classification_rnn.py
+++ s30023/10/lab10_text_classification_rnn.py
@@ -30,8 +30,8 @@ def create_model(encoder):
     model = tf.keras.Sequential([
         encoder,
         tf.keras.layers.Embedding(len(encoder.get_vocabulary()), 64, mask_zero=True),
-        tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64, return_sequences=True, unroll=True)),
-        tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32, unroll=True)),
+        tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64, return_sequences=True)),
+        tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32)),
         tf.keras.layers.Dense(64, activation='relu'),
         tf.keras.layers.Dropout(0.5),
         tf.keras.layers.Dense(1)
